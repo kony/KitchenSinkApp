77e3fd2884e5fef539abb55e4916a499f129898e
Sreenivas <sreenivasulu.nallakshyam@kony.com>
2013-10-17 21:03:52 +0530

    Changes in segmented UI

--- KitchenSinkApp/modules/js/segment.js
+++ KitchenSinkApp/modules/js/segment.js
@@ -40,7 +40,7 @@ function segwidMultiTemplate()
 function SecHdrTemplatesTab()
 {
 	
-		//frmSegmentTablet.segSecHdrTemplates.widgetDataMap = {lblSecHdr1:"lblSecHdr1",lblSecHdr2:"lblSecHdr2",lblBnkAcctName:"lblBnkAcctName",lblWidDrwLimit:"lblWidDrwLimit",lblCreditLimit:"lblCreditLimit",imgChk:"imgChk"};
+		frmSegmentTablet.segSecHdrTemplates.widgetDataMap = {lblSecHdr1:"lblSecHdr1",lblSecHdr2:"lblSecHdr2",lblBnkAcctName:"lblBnkAcctName",lblWidDrwLimit:"lblWidDrwLimit",lblCreditLimit:"lblCreditLimit",imgChk:"imgChk"};
 	
 		frmSegmentTablet.segSecHdrTemplates.data = [
 							[ {lblSecHdr1:"Credit account details",lblSecHdr2:" Account No: xxxxxx0660",template:boxRefSegHdr},					
@@ -75,7 +75,7 @@ function SecHdrTemplatesTab()
 	
 function segWidoutRowtmplateJS()
 {
-	//frmRowTemplates.segWidoutRowtmplate.widgetDataMap = {lbl1:"lbl1",lbl2:"lbl2",lbl3:"lbl3"};
+	frmRowTemplates.segWidoutRowtmplate.widgetDataMap = {lbl1:"lbl1",lbl2:"lbl2",lbl3:"lbl3"};
 	frmRowTemplates.segWidoutRowtmplate.setData([
 					{lbl1: "Titanium Card",lbl2: "$200",lbl3: "$400"}, 
 					{lbl1: "Gold Card",lbl2: "$300",lbl3: "$100"}, 
@@ -102,7 +102,7 @@ function SecHdrWidoutTemplatesJS(evenObj)
 		frmSecHdrWidoutTemplates.lblSegMulSelect.setVisibility(false);		
 		frmSecHdrWidoutTemplates.btnSegMulSelect.setVisibility(false);
 	}
-	//frmSecHdrWidoutTemplates.segSecHdrWidoutTmplate.widgetDataMap = {lblSecHdr1:"lblSecHdr1",lblSecHdr2:"lblSecHdr2",BAccName:"BAccName",WithdrawLimit:"WithdrawLimit",CreditLimit:"CreditLimit",imgChk:"imgChk"};
+	frmSecHdrWidoutTemplates.segSecHdrWidoutTmplate.widgetDataMap = {lblSecHdr1:"lblSecHdr1",lblSecHdr2:"lblSecHdr2",BAccName:"BAccName",WithdrawLimit:"WithdrawLimit",CreditLimit:"CreditLimit",imgChk:"imgChk"};
 	
 	frmSecHdrWidoutTemplates.segSecHdrWidoutTmplate.data = [
 							[ {lblSecHdr1:"Credit account details",lblSecHdr2:" Account No: xxxxxx0660",template:boxRefSegHdr},					
